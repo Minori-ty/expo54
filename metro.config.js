cfa154abf175d2973e378a9df2ddd4dfc1d516e7
天平 <1229248506@qq.com>
2026-01-20 20:28:23 +0800

    feat: 升级到nativewind v5

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config')
-const { withNativeWind } = require('nativewind/metro')
+const { withNativewind } = require('nativewind/metro')
 
 const config = getDefaultConfig(__dirname)
 config.resolver.sourceExts.push('sql')
 
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = withNativewind(config)
